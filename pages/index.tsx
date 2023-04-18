856c621083e78267b1d2c61791d044d368f3f806
Nguyen Canh <canhzone861998@gmail.com>
2023-04-18 21:44:46 +0700

    add mui, mui icon, font roboto

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 export default function Home() {
   return (
     <div className="flex justify-center items-center w-screen h-screen">
+      <div className="flex ">
+        <span className="text-sky-500 font-bold">Test deploy nextjs to vercel</span>
+      </div>
       <p className="text-lg font-thin">
         Hello{' '}
         <span className="text-blue-400 hover:text-blue-900 hover:font-bold cursor-pointer">
